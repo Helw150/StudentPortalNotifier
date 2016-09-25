eb5ab87014520d4c7a5960903d5fbb2d3ceb33f3
wbh230@nyu.edu <wbh230@nyu.edu>
2016-09-25 16:18:30 +0400

    Basic formatting if you are on the StudentPortal ToDo: RemoteAccess for the Portal

--- monitorPage.js
+++ monitorPage.js
@@ -5,20 +5,20 @@ function DOMtoString(document_root) {
     while (node) {
         switch (node.nodeType) {
         case Node.ELEMENT_NODE:
-            html += node.outerHTML;
-            break;
-        case Node.TEXT_NODE:
-            html += node.nodeValue;
-            break;
-        case Node.CDATA_SECTION_NODE:
-            html += '<![CDATA[' + node.nodeValue + ']]>';
-            break;
-        case Node.COMMENT_NODE:
-            html += '<!--' + node.nodeValue + '-->';
-            break;
-        case Node.DOCUMENT_TYPE_NODE:
-            // (X)HTML documents are identified by public identifiers
-            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+	    if(~node.outerHTML.indexOf('data-announcement-id'))
+	    {
+		html += node.outerHTML;
+	    }
+	    if(~node.outerHTML.indexOf('class="announcement"'))
+	    {
+		var events = node.querySelectorAll('li[class="announcement"]');
+		for (var i = 0; i < events.length; ++i)
+		{
+		    var String=events[i].outerHTML.substring(events[i].outerHTML.indexOf('id="announcement-')+17,events[i].outerHTML.indexOf('id="announcement-')+22);
+		    var event = events[i].querySelector('a[href="#"]');
+		    html += '<a href="https://students.nyuad.nyu.edu/announcements/' + String + '">' + event.innerHTML + '</a><br>';
+		}
+	    }
             break;
         }
         node = node.nextSibling;
@@ -30,3 +30,4 @@ chrome.runtime.sendMessage({
     action: "getSource",
     source: DOMtoString(document)
 });
+ 
